4ecbc6820b2855384faec79e33631d9973d0c3a5
dothanhbinh <dothanhbinh11111@gmail.com>
2026-09-17 12:07:33 +0700

    Initial commit

---
+++ react-counter/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+function App() {
+  const [count, setCount] = useState(0);
+
+  function increase() {
+    setCount(count + 1);
+  }
+
+  function decrease() {
+    setCount(count - 1);
+  }
+  return (
+    <>
+      <div>{count}</div>
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
+    </>
+  );
+}
+
+export default App;
